21c823e06c28fec816f9de773ffa7097d5d5327d
shivansh bindal <shivanshbindal603@gmail.com>
2018-10-01 16:59:47 +0530

    improved indentation

--- login.php
+++ login.php
@@ -50,52 +50,52 @@ if(isset($_POST["login"]))
 }
 ?>
 <html>
-<head>
-<title>HARSHIT</title>  
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-<style>  
-body  
-{  
-margin:0;  
-padding:0;  
-        background-color:#f1f1f1;  
-}  
-.box  
-{  
-width:700px;  
-padding:20px;  
-        background-color:#fff;  
-}
+  <head>
+    <title>HARSHIT</title>  
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <style>  
+        body  
+        {  
+        margin:0;  
+        padding:0;  
+                background-color:#f1f1f1;  
+        }  
+        .box  
+        {  
+        width:700px;  
+        padding:20px;  
+                background-color:#fff;  
+        }
 
-</style>  
-</head>  
-<body>  
-<div class="container box">  
-<form action="" method="post" id="frmLogin"> 
-<h3 align="center">Login Details</h3><br />
-<div><?php if(isset($message)) { echo $message; } ?></div>  
-<div>  
-<label for="login">Username</label>  
-<input name="member_name" type="text" value="<?php if(isset($_COOKIE["member_login"])) { 
-$sql = "SELECT name FROM harshit_hashes where hash='".$_COOKIE["member_login"]."' order by id desc";
-$result = $conn->query($sql);
-if($row = $result->fetch_assoc()) { }
- echo $row['name'];} ?>" />  
-</div>  
-<div>  
-<label for="password">Password</label>  
-<input name="member_password" type="password" value="" />   
-</div>  
-<div>  
-<input type="checkbox" name="remember" <?php if(isset($_COOKIE["member_login"])) { ?> checked <?php } ?> id="rem"/>  
-<label for="rem">Remember me</label>  
-</div>  
-<div>  
-<div><input type="submit" name="login" value="Login" ></span></div>  
-</div>   
-</form>  
-<br>  
-<h5>Don't Have an account? Wanna <a href="signup.html">Sign up</a></h5>
-</div>  
-</body>  
+    </style>  
+    </head>  
+    <body>  
+        <div class="container box">  
+        <form action="" method="post" id="frmLogin"> 
+        <h3 align="center">Login Details</h3><br />
+        <div><?php if(isset($message)) { echo $message; } ?></div>  
+        <div>  
+        <label for="login">Username</label>  
+        <input name="member_name" type="text" value="<?php if(isset($_COOKIE["member_login"])) { 
+        $sql = "SELECT name FROM harshit_hashes where hash='".$_COOKIE["member_login"]."' order by id desc";
+        $result = $conn->query($sql);
+        if($row = $result->fetch_assoc()) { }
+        echo $row['name'];} ?>" />  
+        </div>  
+        <div>  
+        <label for="password">Password</label>  
+        <input name="member_password" type="password" value="" />   
+        </div>  
+        <div>  
+        <input type="checkbox" name="remember" <?php if(isset($_COOKIE["member_login"])) { ?> checked <?php } ?> id="rem"/>  
+        <label for="rem">Remember me</label>  
+        </div>  
+        <div>  
+        <div><input type="submit" name="login" value="Login" ></span></div>  
+        </div>   
+        </form>  
+        <br>  
+        <h5>Don't Have an account? Wanna <a href="signup.html">Sign up</a></h5>
+        </div>  
+    </body>  
 </html>
